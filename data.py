8a368630a5a069037e2e5dcada21ac24401c7763
Brian-Ckwu <sonny40920710@gmail.com>
2023-06-29 14:20:58 +0800

    feat(ckwu): add the GIImage class (TODO: GIImage.label_to_seg(.))

---
+++ data.py
@@ -0,0 +1,55 @@
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+class GIImage(object):
+    organs = ["stomach", "small_bowel", "large_bowel"]
+    
+    def __init__(self, fpath: Path, label_df: pd.DataFrame = None):
+        case_day = str(fpath).split('/')[-3]
+        fname = fpath.name # file name
+        metadata = fname.rstrip(".png").split('_')
+        slice_no = metadata[1]
+        numbers = metadata[2:]
+        # metadata: image id
+        self.id = f"{case_day}_slice_{slice_no}"
+        # metadata: slice width/height and pixel width/height
+        self.sw = int(numbers[0])
+        self.sh = int(numbers[1])
+        self.pw = float(numbers[2])
+        self.ph = float(numbers[3])
+        # data: 2D array
+        self.data = plt.imread(fpath)
+        if label_df is not None:
+            self.labels = self.get_labels(label_df)
+    
+    def get_labels(self, label_df: pd.DataFrame) -> dict:
+        labels = label_df.loc[label_df.id == self.id]
+        organ2label = dict()
+        for _, row in labels.iterrows():
+            organ2label[row["class"]] = self.seg_to_label(row["segmentation"])
+        return organ2label
+
+    # converting run-length encoding to pixel-wise labels
+    def seg_to_label(self, seg: str):
+        label = np.zeros(shape=(self.sw, self.sh))
+        if type(seg) == str:
+            numbers = seg.split(' ')
+            assert len(numbers) % 2 == 0
+            for i in range(0, len(numbers), 2):
+                start_id = int(numbers[i])
+                length = int(numbers[i + 1])
+                for j in range(length):
+                    pixel = start_id + j
+                    px = math.ceil(pixel / self.sw)
+                    py = ((pixel - 1) % self.sw) + 1
+                    label[px, py] = 1
+        return label
+
+    def label_to_seg(self, label):
+        raise NotImplementedError
+    
+    def print_image_info(self) -> None:
+        print(f"Image ID: {self.id}; slice width/height = ({self.sw}, {self.sh}); data shape = {self.data.shape}")
