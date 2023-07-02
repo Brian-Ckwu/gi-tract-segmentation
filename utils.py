9458af6c3d464213b6110ab837020e49e60f4b37
Brian-Ckwu <sonny40920710@gmail.com>
2023-07-02 13:52:12 +0800

    feat(ckwu): add set_seed(.)

--- utils.py
+++ utils.py
@@ -1,7 +1,19 @@
 import yaml
 from argparse import Namespace
 
+import random
+import numpy as np
+import torch
+
 def load_yaml(file: str) -> Namespace:
     with open(file) as f:
         config = yaml.safe_load(f)
     return Namespace(**config)
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
