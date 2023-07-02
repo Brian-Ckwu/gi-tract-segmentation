6eb5c6be205a5df1411c11b42cce451f51b49d93
Brian-Ckwu <sonny40920710@gmail.com>
2023-07-02 14:37:00 +0800

    feat(ckwu): add GITractDataset and train_valid_split functionality

--- data.py
+++ data.py
@@ -1,9 +1,12 @@
+import os
 import math
+import random
 import torch
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+from torch.utils.data import Dataset
 
 class GIImage(object):
     organs = ["stomach", "small_bowel", "large_bowel"]
@@ -68,3 +71,53 @@ class GIImage(object):
             if self.labels:
                 axs[0, i].imshow(self.labels[organ], cmap="gray", alpha=0.4)
             axs[0, i].set_title(organ)
+
+class GITractDataset(Dataset):
+    
+    def __init__(
+        self,
+        image_path: Path,
+        label_path: Path = None,
+        cases: set[str] = None
+    ):
+        if cases:
+            self._image_paths = self.get_image_files_by_cases(image_path, cases)
+        else:
+            self._image_paths = [fpath for fpath in self.image_files_walker(image_path)]
+        self._label_df = None
+        if label_path:
+            self._label_df = pd.read_csv(label_path)
+    
+    def __len__(self):
+        return len(self._image_paths)
+    
+    def __getitem__(self, idx: int):
+        return GIImage(fpath=self._image_paths[idx], label_df=self._label_df)
+    
+    @staticmethod
+    def get_image_files_by_cases(image_path: str, cases: set[str]) -> list:
+        image_path = Path(image_path)
+        image_paths = []
+        for case in os.listdir(image_path):
+            if case in cases:
+                case_path = image_path / case
+                for day in os.listdir(case_path):
+                    day_path = case_path / day / "scans"
+                    for file in os.listdir(day_path):
+                        fpath = day_path / file
+                        image_paths.append(fpath)
+        return image_paths
+
+    @staticmethod
+    def image_files_walker(image_path: str):
+        for dirname, _, filenames in os.walk(image_path):
+            dirpath = Path(dirname)
+            for filename in filenames:
+                yield dirpath / filename
+
+def train_valid_split_cases(image_path: str, valid_size: float = 0.2) -> set:
+    cases = os.listdir(image_path)
+    valid_cases = set(random.sample(cases, math.ceil(len(cases) * valid_size)))
+    train_cases = set(cases) - valid_cases
+    assert ((valid_cases & train_cases) == set()) and ((valid_cases | train_cases) == set(cases))
+    return train_cases, valid_cases
