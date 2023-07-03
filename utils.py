706f3a853627ec3ec1936893dbb45a9100320b45
Brian-Ckwu <sonny40920710@gmail.com>
2023-07-03 09:06:40 +0000

    feat(ckwu): add show_valid_image_during_training(.)

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import random
 import numpy as np
 import torch
 import torch.nn as nn
+import torchvision.transforms.functional as TF
+from matplotlib.figure import Figure
 
 def load_yaml(file: str) -> Namespace:
     with open(file) as f:
@@ -26,3 +28,16 @@ def get_output_shape(model: nn.Module, input_shape: tuple):
 def filename_to_width_height(filename: str) -> tuple:
     splitted = filename.split('_')
     return int(splitted[2]), int(splitted[3])
+
+def show_valid_image_during_training(model: nn.Module, image, input_resolution: int, padding_mode: str, device: str) -> Figure:
+    padding = ((input_resolution - image.sw) // 2, (input_resolution - image.sh) // 2)
+    image_tensor = TF.pad(image.tensor, padding=padding, padding_mode=padding_mode).unsqueeze(0).unsqueeze(0).to(device)
+    model = model.to(device)
+    preds = model(image_tensor)
+    segmentations = dict()
+    for i, organ in enumerate(image.organs):
+        pred = preds[:, i, :, :]
+        segmentation = TF.center_crop(pred, output_size=(image.sh, image.sw))
+        segmentations[organ] = segmentation.squeeze(0).detach().cpu().numpy()
+    fig = image.show_segmented_images(segmentations=segmentations)
+    return fig
